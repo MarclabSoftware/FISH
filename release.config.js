dcb6bc283cb2ea6c3eadea941ecf51eb8225562c
Leonardo Marcucci <marcuson.nn90@gmail.com>
2022-03-11 19:04:13 +0100

    style: Cosmetics.

--- release.config.js
+++ release.config.js
@@ -1,43 +1,43 @@
 const ref = process.env.GITHUB_REF;
-const branch = ref ? ref.split("/").pop() : "main";
+const branch = ref ? ref.split('/').pop() : 'main';
 
 const config = {
   branches: [
-    "+([0-9])?(.{+([0-9]),x}).x",
-    "main",
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
     {
-      name: "beta",
-      prerelease: "beta",
+      name: 'beta',
+      prerelease: 'beta',
     },
     {
-      name: "alpha",
-      prerelease: "alpha",
+      name: 'alpha',
+      prerelease: 'alpha',
     },
   ],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
   ],
 };
 
 config.plugins.push([
-  "@semantic-release/changelog",
+  '@semantic-release/changelog',
   {
     changelogFile: `docs/CHANGELOG-${branch}.md`,
   },
 ]);
 
 config.plugins.push([
-  "@semantic-release/git",
+  '@semantic-release/git',
   {
-    assets: ["docs"],
+    assets: ['docs'],
     message:
-      "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
   },
 ]);
 
-if (branch != "alpha") {
-  config.plugins.push("@semantic-release/github");
+if (branch != 'alpha') {
+  config.plugins.push('@semantic-release/github');
 }
 
 module.exports = config;
